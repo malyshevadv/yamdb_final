af6092d14e3f5d6bf2b87ea46069c88339d3619d
Darya <lunaticmoony13@gmail.com>
2022-02-06 23:25:15 +0300

    fixing static

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -68,7 +68,7 @@ WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME', default='postgres'),
+        'NAME': os.getenv('DB_NAME', default='yatube'),
         'USER': os.getenv('POSTGRES_USER', default='postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default=''),
         'HOST': os.getenv('DB_HOST', default='db'),
