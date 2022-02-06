a1360114895ac964896fe1be258c96249c28f359
Darya <lunaticmoony13@gmail.com>
2022-02-06 22:13:56 +0300

    figuring out .env

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -69,7 +69,7 @@ DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
         'NAME': os.getenv('DB_NAME', default='postgres'),
-        'USER': os.getenv('POSTGRES_USER', default='postgres'),
+        'USER': os.getenv('POSTGRES_USER', default=''),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default=''),
         'HOST': os.getenv('DB_HOST', default='db'),
         'PORT': os.getenv('DB_PORT', default='5432')
